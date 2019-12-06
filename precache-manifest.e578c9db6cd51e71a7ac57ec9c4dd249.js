36ded2f68abbd190d1681f03d5fa7fdc499da48a
dteiml <dteiml@gmail.com>
2019-12-06 10:26:07 +0100

    Create gh-pages branch with static content

---
+++ precache-manifest.e578c9db6cd51e71a7ac57ec9c4dd249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f3a52c4f0d251fb19a4eb1392d0955",
+    "url": "/amos-pwa/index.html"
+  },
+  {
+    "revision": "48fd5894983391e6ac72",
+    "url": "/amos-pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23842fe108af525e12b3",
+    "url": "/amos-pwa/static/js/2.329f94f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/amos-pwa/static/js/2.329f94f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48fd5894983391e6ac72",
+    "url": "/amos-pwa/static/js/main.796be9b0.chunk.js"
+  },
+  {
+    "revision": "73e8731c7830e4a4c679",
+    "url": "/amos-pwa/static/js/runtime-main.57e2e3aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amos-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
