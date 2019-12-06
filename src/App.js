f4d465afa5f153eb0f1a44c3ff5c44da019ed2f3
dteiml <dteiml@gmail.com>
2019-12-06 11:13:57 +0100

    add input

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const [text, setText] = React.useState (``)
+
+  React.useEffect(() => {
+    setText (localStorage.getItem (`text`))
+  }, [])
+
+  const handleChange = (e) => {
+    localStorage.setItem (`text`, e.target.value)
+    setText (e.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn PWA
-        </a>
-      </header>
+      <p>You typed: {text}</p>
+      <input onChange={handleChange}></input>
     </div>
   );
 }
